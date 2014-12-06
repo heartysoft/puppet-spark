1cf453252bf7b0e6b3114280d43479bdc860fddb
ashic <ashic@live.com>
2014-12-06 20:23:46 +0000

    better subscription for file changes in service

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,39 +10,35 @@ class spark::service(
 	){
 
 	if $mode == 'master' {
-		
 		file { "$install_dir/spark/conf/spark-env.sh":
 			ensure => present,
 			content => template('spark/spark-env.sh.master.erb'),
 			mode => '0755',
-			notify => Service['spark'],
 		}
 		->
 		file { '/etc/init.d/spark':
 			ensure => present,
 			content => template('spark/init.d.master.erb'),
 			mode => '0755',
-			notify => Service['spark'],
 		}
 	} else {
 		file { "$install_dir/spark/conf/spark-env.sh":
 			ensure => present,
 			content => template('spark/spark-env.sh.worker.erb'),
 			mode => '0755',
-			notify => Service['spark'],
 		}
-		->
+		->		
 		file { '/etc/init.d/spark':
 			ensure => present,
 			content => template('spark/init.d.worker.erb'),
 			mode => '0755',
-			notify => Service['spark'],
 		}
 	}
 
 	service { 'spark':
 		ensure => running,
 		enable => true,
-		require => File['/etc/init.d/spark'],
+		require => File['/etc/init.d/spark', "$install_dir/spark/conf/spark-env.sh"],
+		subscribe => File['/etc/init.d/spark', "$install_dir/spark/conf/spark-env.sh"],
 	}
 }
